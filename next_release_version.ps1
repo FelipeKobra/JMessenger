31c6ce4bf03eaad7b1f37a34ba67b54bf035f5f5
Felipe Kobra <139709626+FelipeKobra@users.noreply.github.com>
2025-02-12 23:43:54 -0300

    refactor: Translate variables and remove comments

--- next_release_version.ps1
+++ next_release_version.ps1
@@ -1,43 +1,33 @@
-# Defina a versão atual
 $versao_atual = git describe --tags --abbrev=0
 
-# Remova o caractere "v" do início da string
 $versao_atual = $versao_atual -replace '^v', ''
 
-# Defina as variáveis para contagem de commits
 $major, $minor, $patch = $versao_atual -split '\.'
 $major = [int]$major
 $minor = [int]$minor
 $patch = [int]$patch
 
-# Obtenha os commits desde a versão atual
 $commits = git log --since="$versao_atual" --reverse --format=%s
 
-# Itere sobre os commits
 foreach ($commit in $commits)
 {
-    # Verifique se o commit é um breaking change
     if ($commit -match "^BREAKING")
     {
         $major += 1
         $minor = 0
         $patch = 0
     }
-    # Verifique se o commit é um feat
     elseif ($commit -match "^feat")
     {
         $minor += 1
         $patch = 0
     }
-    # Verifique se o commit é um fix
     elseif ($commit -match "^fix")
     {
         $patch += 1
     }
 }
 
-# Calcule a próxima versão
-$proxima_versao = "$major.$minor.$patch"
+$next_version = "$major.$minor.$patch"
 
-# Imprima a próxima versão
-Write-Host "Próxima versão: $proxima_versao"
+Write-Host "Next release version: $next_version"
