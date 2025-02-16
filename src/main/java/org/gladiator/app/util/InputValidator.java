793e3551889dd59419acf2699218257802db7c2d
Felipe Kobra <139709626+FelipeKobra@users.noreply.github.com>
2025-02-16 20:45:40 -0300

    feat: create InputValidator

---
+++ src/main/java/org/gladiator/app/util/InputValidator.java
@@ -0,0 +1,29 @@
+package org.gladiator.app.util;
+
+/**
+ * Utility class for validating user input.
+ */
+public final class InputValidator {
+
+  /**
+   * Maximum length for a user name.
+   */
+  public static final int USER_NAME_MAX_LENGTH = 40;
+
+  /**
+   * Private constructor to prevent instantiation.
+   */
+  private InputValidator() {
+  }
+
+  /**
+   * Validates the given username.
+   *
+   * @param userName the username to validate
+   * @return {@code true} if the username is not empty, not null, and does not exceed the maximum
+   * length; {@code false} otherwise
+   */
+  public static boolean isUserNameValid(final String userName) {
+    return !"".equals(userName) && null != userName && USER_NAME_MAX_LENGTH >= userName.length();
+  }
+}
